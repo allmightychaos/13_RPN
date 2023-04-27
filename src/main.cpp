6d5bd6062652b9a192023efaedec4d65d699dfe7
Chaos <weghofer.sam@gmail.com>
2023-04-27 12:33:15 +0200

    Changed Output Format

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "calculator.h"
 
 using namespace std;
 
 int main()
 {
+    system("clear");
     Calculator calc;
     double res;
     string line;
     string p_input;
+    const int max_calc_length = 10;
+    const int total_length = 15;
 
     cout << "Entering 'q' or 'stop' will stop." << endl;
 
@@ -22,13 +26,20 @@ int main()
             continue;
         }
         else if (line == "q" || line == "stop") {
+            system("clear");
             break;
         }
         else {
             calc = Calculator();
             p_input = line;
             res = calc.calculate(p_input);
-            cout << line << " " << res << endl;
+
+            string truncated_input = p_input.substr(0, max_calc_length);
+            if (p_input.length() > max_calc_length) {
+                truncated_input = truncated_input.substr(0, truncated_input.length() - 2) + "...";
+            }
+
+            cout << setw(total_length - 2) << left << truncated_input << "= " << res << "\n" << endl;
         }
     }
 
