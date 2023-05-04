5e3f49a4a1ac6f06ff0b68110d7983a2d6226c46
Chaos <weghofer.sam@gmail.com>
2023-05-04 11:08:58 +0200

    Added Catch For Errors

--- src/main.cpp
+++ src/main.cpp
@@ -18,28 +18,38 @@ int main()
     cout << "Entering 'q' or 'stop' will stop." << endl;
 
     while (true) {
-        cout << "> ";
-        getline(cin, line);
+        try {
+            cout << "> ";
+            getline(cin, line);
 
-        if (line == "clear") {
-            system("clear");
+            if (line == "clear") {
+                system("clear");
+                continue;
+            }
+            else if (line == "q" || line == "stop") {
+                system("clear");
+                break;
+            }
+            else {
+                calc = Calculator();
+                p_input = line;
+                res = calc.calculate(p_input);
+
+                string truncated_input = p_input.substr(0, max_calc_length);
+                if (p_input.length() > max_calc_length) {
+                    truncated_input = truncated_input.substr(0, truncated_input.length() - 2) + "...";
+                }
+
+                cout << setw(total_length - 2) << left << truncated_input << "= " << res << "\n" << endl;
+            }
+        }
+        catch (const std::invalid_argument) {
+            cout << "Invalid input. Try again!" << endl;
             continue;
         }
-        else if (line == "q" || line == "stop") {
-            system("clear");
-            break;
-        }
-        else {
-            calc = Calculator();
-            p_input = line;
-            res = calc.calculate(p_input);
-
-            string truncated_input = p_input.substr(0, max_calc_length);
-            if (p_input.length() > max_calc_length) {
-                truncated_input = truncated_input.substr(0, truncated_input.length() - 2) + "...";
-            }
-
-            cout << setw(total_length - 2) << left << truncated_input << "= " << res << "\n" << endl;
+        catch (const std::logic_error) {
+            cout << "Error: Invalid RPN expression. Try again!" << endl;
+            continue;
         }
     }
 
